13768c17084e64d9902ae7f0cbe5b07f95566cb9
Fengbai Han <fengbai.han@outlook.com>
2023-03-22 19:36:59 -0400

    compelted

--- merge_files.py
+++ merge_files.py
@@ -9,7 +9,10 @@ brm_df = pd.read_csv(os.path.join(f_path, 'BRM.csv'))
 babiness_df = pd.read_csv(os.path.join(f_path, 'Iconicity.csv'))
 esm_df = pd.read_csv(os.path.join(f_path, 'ESM.csv'))
 
-merge_df = pd.concat([AoA_df, brm_df, babiness_df, esm_df])
+merge_df = pd.merge(AoA_df, brm_df, on='Word', how='outer')
+merge_df = pd.merge(merge_df, babiness_df, on='Word', how='outer')
+merge_df = pd.merge(merge_df, esm_df, on='Word', how='outer')
+
 merge_df.to_csv(os.path.join(f_path, "merged.csv"),
                 index = None,
                 header=True)
